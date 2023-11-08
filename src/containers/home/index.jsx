a07a376cba79fc29fd42c40f0f85a1a91f020a6f
SriMahith27 <143414137+SriMahith27@users.noreply.github.com>
2023-11-08 16:48:50 +0530

    Update index.jsx

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <section id="home" className="home">
       <div className="home__text-wrapper">
         <h1>
-          Hello, I'm Sravan
+          Hi,I Am Mahith
           <br />
           Front end developer
         </h1>
